91f01cdf9bbbbef0e9406e28b1342eb3570066e2
stefaleon <stefaleon@gmail.com>
2020-04-13 19:27:51 +0300

    book model

---
+++ src/book.ts
@@ -0,0 +1,25 @@
+import * as mongoose from 'mongoose';
+
+const uri: string =
+  'mongodb+srv://leon123:leon123@cluster0-sxvew.mongodb.net/books?retryWrites=true&w=majority';
+
+mongoose.connect(uri, (err: any) => {
+  if (err) {
+    console.log(err.message);
+  } else {
+    console.log('Successfully Connected!');
+  }
+});
+
+export interface IBook extends mongoose.Document {
+  title: string;
+  author: number;
+}
+
+export const BookSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  author: { type: String, required: true },
+});
+
+const Book = mongoose.model<IBook>('Book', BookSchema);
+export default Book;
