c31b5ad0ac02e2078e8d5f909eaeca67fc5693f2
stefaleon <stefaleon@gmail.com>
2020-04-13 19:55:12 +0300

    CRUD methods

--- src/controllers/bookController.ts
+++ src/controllers/bookController.ts
@@ -12,17 +12,48 @@ export let allBooks = (req: Request, res: Response) => {
 };
 
 export let getBook = (req: Request, res: Response) => {
-  res.send('Returns one book');
+  let book = Book.findById(req.params.id, (err: any, book: any) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(book);
+    }
+  });
 };
 
 export let deleteBook = (req: Request, res: Response) => {
-  res.send('Returns one book');
+  let book = Book.deleteOne({ _id: req.params.id }, (err: any) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send('Successfully Deleted Book');
+    }
+  });
 };
 
 export let updateBook = (req: Request, res: Response) => {
-  res.send('Returns one book');
+  console.log(req.body);
+  let book = Book.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    (err: any, book: any) => {
+      if (err) {
+        res.send(err);
+      } else {
+        res.send('Successfully updated book!');
+      }
+    }
+  );
 };
 
 export let addBook = (req: Request, res: Response) => {
-  res.send('Returns one book');
+  var book = new Book(req.body);
+
+  book.save((err: any) => {
+    if (err) {
+      res.send(err);
+    } else {
+      res.send(book);
+    }
+  });
 };
